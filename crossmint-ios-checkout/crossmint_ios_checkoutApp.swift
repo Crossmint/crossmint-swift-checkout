876311395a6b3830efc2b5cd904985de8a78e5f3
jcurbelo <robin@paella.dev>
2025-11-13 15:44:42 -0500

    Initial Commit

---
+++ crossmint-ios-checkout/crossmint_ios_checkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  crossmint_ios_checkoutApp.swift
+//  crossmint-ios-checkout
+//
+//  Created by Robin Curbelo on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct crossmint_ios_checkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ crossmint-ios-checkoutUITests/crossmint_ios_checkoutUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  crossmint_ios_checkoutUITestsLaunchTests.swift
+//  crossmint-ios-checkoutUITests
+//
+//  Created by Robin Curbelo on 11/13/25.
+//
+
+import XCTest
+
+final class crossmint_ios_checkoutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
